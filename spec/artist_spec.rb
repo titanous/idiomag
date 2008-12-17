8a9b557f42bfa149a1451f4822289d7dfa580e7e
Jonathan Rudenberg <jon335@gmail.com>
2008-12-17 12:43:38 -0500

    added prefetch tests

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -22,11 +22,15 @@ describe 'Idiomag::Artist' do
     @artist.respond_to?(:playlist).should == true
     @artist.respond_to?(:tracks).should == true
   end
-  
+
   describe 'info' do
     before(:each) do
       @data = open(Fixtures + 'artist_info.json').read
       @artist.should_receive(:fetch).and_return(JSON.parse(@data))
+    end
+    
+    it 'should allow prefetching' do
+      @artist.get(:info)
     end
     
     it 'should get links' do
@@ -44,6 +48,10 @@ describe 'Idiomag::Artist' do
       @artist.should_receive(:fetch).and_return(JSON.parse(@data))
     end
     
+    it 'should allow prefetching' do
+      @artist.get(:tags)
+    end
+    
     it 'should get tags' do
       @artist.tags[:alternative_rock].should == 0.37
     end
@@ -53,6 +61,10 @@ describe 'Idiomag::Artist' do
     before(:each) do
       @data = open(Fixtures + 'artist_articles.json').read
       @artist.should_receive(:fetch).and_return(JSON.parse(@data))
+    end
+    
+    it 'should allow prefetching' do
+      @artist.get(:articles)
     end
     
     it 'should get articles' do
@@ -72,6 +84,10 @@ describe 'Idiomag::Artist' do
       @artist.should_receive(:fetch).and_return(JSON.parse(@data))
     end
     
+    it 'should allow prefetching' do
+      @artist.get(:photos)
+    end
+    
     it 'should get photos' do
       @artist.photos[0][:title].should =~ /jpg/
       @artist.photos[0][:url].should =~ /jpg/
@@ -84,10 +100,14 @@ describe 'Idiomag::Artist' do
     end
   end
   
-  describe 'videos' do
+  describe 'videos' do    
     before(:each) do
       @data = open(Fixtures + 'artist_videos.json').read
       @artist.should_receive(:fetch).and_return(JSON.parse(@data))
+    end
+    
+    it 'should allow prefetching' do
+      @artist.get(:videos)
     end
     
     it 'should get videos' do
@@ -103,11 +123,16 @@ describe 'Idiomag::Artist' do
       @artist.should_receive(:fetch).and_return(JSON.parse(@data))
     end
     
+    it 'should allow prefetching' do
+      @artist.get(:playlist)
+    end
+    
     it 'should get playlist' do
       @artist.playlist[0][:location] =~ /mp3/
     end
     
     it 'should respond to #tracks' do
+      @artist.get(:tracks)
       @artist.tracks[0][:location] =~ /mp3/
     end
   end
