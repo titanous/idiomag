0d806b262af6cd403a9e1baa2179b1754687e692
Jonathan Rudenberg <jon335@gmail.com>
2008-12-19 11:40:22 -0500

    replaced p with puts

--- examples/artist.rb
+++ examples/artist.rb
@@ -3,30 +3,30 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'idiomag
 Idiomag::Base.api_key = 'foo' # get this @ idiomag.com/api
 artist = Idiomag::Artist.new('Anberlin')
 
-p "#{artist.name}'s URLs:"
-artist.links.each {|link| p link}
-p "\n\n"
+puts "#{artist.name}'s URLs:"
+artist.links.each {|link| puts link}
+puts "\n\n"
 
-p "#{artist.name}'s related artists:"
-artist.related.each {|name,link| p "#{name}: #{link}"}
-p "\n\n"
+puts "#{artist.name}'s related artists:"
+artist.related.each {|name,link| puts "#{name}: #{link}"}
+puts "\n\n"
 
-p "#{artist.name}'s tags:"
-artist.tags.each {|tag,value| p "#{tag} = #{value}"}
-p "\n\n"
+puts "#{artist.name}'s tags:"
+artist.tags.each {|tag,value| puts "#{tag} = #{value}"}
+puts "\n\n"
 
-p "#{artist.name}'s recent articles:"
-artist.articles.each {|article| p "#{article[:title]} on #{article[:date].strftime('%A')}"}
-p "\n\n"
+puts "#{artist.name}'s recent articles:"
+artist.articles.each {|article| puts "#{article[:title]} on #{article[:date].strftime('%A')}"}
+puts "\n\n"
 
-p "#{artist.name}'s photos:"
-artist.photos.each {|photo| p photo[:url]}
-p "\n\n"
+puts "#{artist.name}'s photos:"
+artist.photos.each {|photo| puts photo[:url]}
+puts "\n\n"
 
-p "#{artist.name}'s videos:"
-artist.videos.each {|video| p "#{video[:title]} - #{video[:location]}"}
-p "\n\n"
+puts "#{artist.name}'s videos:"
+artist.videos.each {|video| puts "#{video[:title]} - #{video[:location]}"}
+puts "\n\n"
 
-p "#{artist.name}'s tracks:"
-artist.tracks.each {|track| p "#{track[:title]} - #{track[:location]}"}
-p "\n\n"
+puts "#{artist.name}'s tracks:"
+artist.tracks.each {|track| puts "#{track[:title]} - #{track[:location]}"}
+puts "\n\n"
