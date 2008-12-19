b77b25cd3c734ed40b1af811c752b1ce4f666f64
Jonathan Rudenberg <jon335@gmail.com>
2008-12-19 11:23:51 -0500

    minor changes

--- examples/artist.rb
+++ examples/artist.rb
@@ -5,21 +5,28 @@ artist = Idiomag::Artist.new('Anberlin')
 
 p "#{artist.name}'s URLs:"
 artist.links.each {|link| p link}
+p "\n\n"
 
 p "#{artist.name}'s related artists:"
 artist.related.each {|name,link| p "#{name}: #{link}"}
+p "\n\n"
 
 p "#{artist.name}'s tags:"
 artist.tags.each {|tag,value| p "#{tag} = #{value}"}
+p "\n\n"
 
 p "#{artist.name}'s recent articles:"
 artist.articles.each {|article| p "#{article[:title]} on #{article[:date].strftime('%A')}"}
+p "\n\n"
 
 p "#{artist.name}'s photos:"
 artist.photos.each {|photo| p photo[:url]}
+p "\n\n"
 
-p "#{artist.name}'s videos"
+p "#{artist.name}'s videos:"
 artist.videos.each {|video| p "#{video[:title]} - #{video[:location]}"}
+p "\n\n"
 
-p "#{artist.name}'s tracks"
+p "#{artist.name}'s tracks:"
 artist.tracks.each {|track| p "#{track[:title]} - #{track[:location]}"}
+p "\n\n"
